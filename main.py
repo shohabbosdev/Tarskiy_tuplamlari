07fe517e26a1760caa4f5e5f2e63a0e19af6e258
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2025-04-26 14:27:36 +0500

    oraliq kattalashdi

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ from time import *
 
 set_page_config("Tarskiy to'plamlari",'⚙️', 'wide', 'expanded')
 title("Tarskiy-Kuratskiy to'plamlari (Sonning bo'luvchilarini topish dasturi)")
-n=select_slider("Sonlarni kiriting", options=[i for i in range(10_001)])
+n=select_slider("Sonlarni kiriting", options=[i for i in range(1_000_001)])
+
+tab1,tab2 = tabs(["1-oyna Sonlarning bo'luvchilari","2-oyna Murakkab sonlar"])
+
 start = time()
-with expander("Natijalar pastda", icon='🪫'):
-        json([i for i in range(1,n+1) if n%i==0])
-end = time()-start
-write(f"Ishlash vaqti: {end*1_000:.3f} millisekund")
+with tab1:
+    with expander("Natijalar pastda", icon='🪫'):
+            json([i for i in range(1,n+1) if n%i==0])
+    write(sum([i for i in range(1,n+1) if n%i==0]))
+    write(sum([i for i in range(1,n+1) if n%i==0])-n==n)
+    end = time()-start
+    vaqt = f"{end*1_000:.3f}"
+    end1 = end-time()
+
+with tab2:
+    k=0
+    for j in range(1,n+1):
+        murakkab=sum([i for i in range(1,j+1) if j%i==0])-j
+        if murakkab==j:
+            k+=1
+            json({
+                k:j
+            })
+
+metric("Vaqt farqi", vaqt, (end1-end))
