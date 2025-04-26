411b2d97257ca8d4604befc7bd20493c369a6674
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2025-04-26 13:53:46 +0500

    slider update

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from streamlit import *
 from time import *
 
-title("Torskiy-Kuratskiy to'plamlari (Sonning bo'luvchilarini topish dasturi)")
-n=select_slider("Sonlarni kiriting", options=[i for i in range(10001)])
-running_time = []
-
+title("Tarskiy-Kuratskiy to'plamlari (Sonning bo'luvchilarini topish dasturi)")
+n=select_slider("Sonlarni kiriting", options=[i for i in range(10_001)])
 start = time()
-json(([i for i in range(1,n+1) if n%i==0]))
-write(sum([i for i in range(1,n+1) if n%i==0]))
+with expander("Natijalar pastda", icon='🪫'):
+        json([i for i in range(1,n+1) if n%i==0])
 end = time()-start
-running_time.append(end)
-end1 = time()-end
-end = f"{end*1_000:.2f}"
-metric("Ishlash vaqti", end, running_time[-1]/end1)
+write(f"Ishlash vaqti: {end*1_000:.3f} millisekund")
